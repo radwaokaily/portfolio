82bfc94de305cd264da10f0776ae4696bf2a66d2
radwa okaily <74832631+radwaokaily@users.noreply.github.com>
2025-01-13 02:13:41 +0200

    them

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./contact.css"
+import { useForm, ValidationError } from '@formspree/react';
+
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xyzzozor");
+  
   return (
     <section className="contact-us d-flex">
       <h3 className='d-flex'>
@@ -9,20 +13,34 @@ const Contact = () => {
       </h3>
       <p>Contact me for more information</p>
       <div className="contact-us-form d-flex">
-
-      <form action="" className=''>
+    <div className="form">
+      <form method='POST' action='https://formspree.io/f/xyzzozor' onSubmit={handleSubmit} className=''>
         <div className="email d-flex">
 
       <label htmlFor="email">Email Address</label>
-      <input type="email" name="email" id="email" />
+      <input  type="email" name="email" id="email" />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
         </div>
         <div className="textarea d-flex">
           <label htmlFor="message">Your Message</label>
-      <textarea name="message" id="message"></textarea>
+      <textarea  name="message" id="message"></textarea>
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
         </div>
       
-      <button>Submit</button>
+      <button  type="submit" disabled={state.submitting}>Submit</button>
       </form>
+      <div className='succeeded-message'>
+        {state.succeeded && <h2>Thanks for contact me</h2>}
+      </div>
+    </div>
       <div className="animation-form"></div>
       </div>
     </section>
